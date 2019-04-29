a3bba8c7f967a877f99e8a8d9b37e201533c7d89
Artem Antuh <artemantuh@192.168.100.4>
2019-04-29 23:14:25 +0300

    Дизайн главного экрана приложения

---
+++ Help/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Help
+//
+//  Created by Artem Antuh on 29/04/2019.
+//  Copyright © 2019 Artem Antuh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
